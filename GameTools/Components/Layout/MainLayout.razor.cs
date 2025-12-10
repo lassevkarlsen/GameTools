38317596ea6f4eee61965f42e896a6dfdbc8414c
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-11 00:02:02 +0100

    Display commit ID in footer with link to GitHub and integrate version tracking
    Added logic to fetch the Git commit ID from a file during initialization and display it as

--- GameTools/Components/Layout/MainLayout.razor.cs
+++ GameTools/Components/Layout/MainLayout.razor.cs
@@ -2,4 +2,16 @@
 
 public partial class MainLayout
 {
+    private string? _version;
+
+    protected override void OnInitialized()
+    {
+        string idFileName = "git_id.txt";
+        if (File.Exists(idFileName))
+        {
+            _version = File.ReadAllText(idFileName);
+        }
+
+        base.OnInitialized();
+    }
 }
