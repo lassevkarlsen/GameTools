bc95fc4588562ccf9dd30c32921fe641e4bf62b7
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2026-05-09 11:53:38 +0200

    Add Diablo 4 Event Timers page and menu integration

---
+++ GameTools/Components/Pages/Diablo4/Diablo4EventTimers.razor.cs
@@ -0,0 +1,13 @@
+﻿using GameTools.Components.Pages.Enshrouded;
+
+namespace GameTools.Components.Pages.Diablo4;
+
+public partial class Diablo4EventTimers
+{
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        SetPageTitle?.Invoke("Diablo 4 :: Event Timers");
+    }
+}
